64d1671b3a6db40fd98ec8b9b0f1153eb9089db0
Unknown <coolzhye@gmail.com>
2018-06-01 16:48:52 -0400

    Storage Fragment

--- app/src/main/java/com/coolzhye/androidexercise1/StorageFragment.java
+++ app/src/main/java/com/coolzhye/androidexercise1/StorageFragment.java
@@ -1,11 +1,14 @@
 package com.coolzhye.androidexercise1;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -13,17 +16,59 @@ import android.view.ViewGroup;
  */
 public class StorageFragment extends Fragment {
 
+    private final String KEY_NUMBER = "number";
+    private int number = 0;
 
     public StorageFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_storage, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_storage, container, false);
+
+        rootView.findViewById(R.id.btnMinus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateNumber(--number);
+            }
+        });
+
+        rootView.findViewById(R.id.btnPlus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateNumber(++number);
+            }
+        });
+
+        return rootView;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("ORIENTATION", "onResume");
+
+        SharedPreferences sharedPreferences = getActivity().getPreferences(getContext().MODE_PRIVATE);
+        number = sharedPreferences.getInt(KEY_NUMBER, 0);
+        updateNumber(number);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d("ORIENTATION", "onPause");
+        Log.d("ORIENTATION", (getResources().getConfiguration().orientation == 1 ? "PORTRAIT - " : "LANDSCAPE - ") + Integer.toString(number));
+        SharedPreferences sharedPreferences = getActivity().getPreferences(getContext().MODE_PRIVATE);
+        sharedPreferences.edit().putInt(KEY_NUMBER, number).commit();
+    }
+
+    private void updateNumber(int newNumber) {
+        number = newNumber;
+
+        TextView textViewNumber = getView().findViewById(R.id.textViewNumber);
+        textViewNumber.setText(Integer.toString(newNumber));
+    }
 }
